c321b2d0cbdccd58007465cd132cdf28aff1c192
zinefer <zinefer@gmail.com>
2019-12-23 20:27:35 -0700

    Replace generated homepage

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-container>
+      <v-layout text-center wrap>
+        <v-flex xs12>
+          <v-img
+            :src="require('../assets/logo.svg')"
+            class="my-3"
+            contain
+            height="200"
+          ></v-img>
+        </v-flex>
+
+        <v-flex mb-4>
+          <h1 class="display-2 font-weight-bold mb-3">
+            Welcome to Habits
+          </h1>
+          <p class="subheading font-weight-regular">
+            This website can help track streaks in your habits.
+          </p>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
