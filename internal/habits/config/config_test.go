19deba8b041995272131318a6e499cf00f3da94d
zinefer <zinefer@gmail.com>
2019-12-29 22:34:08 -0700

    Move config/database_test into config/config_test

--- internal/habits/config/config_test.go
+++ internal/habits/config/config_test.go
@@ -15,16 +15,36 @@ type TestSuite struct {
 
 func (suite *TestSuite) TestConfiguration() {
 	config.DatabaseConfigPath = "testconfig.yml"
-
-	var configuration *config.Configuration
-	assert.NotPanics(suite.T(), func() {
-		configuration = config.New()
-	}, "Does not panic with a good config")
-
+	configuration := config.New()
 	assert.Equal(suite.T(), "127.0.0.1", configuration.Database.Host)
 	assert.Equal(suite.T(), "habits_development", configuration.Database.Name)
 	assert.Equal(suite.T(), "postgres", configuration.Database.Username)
 }
+
+func (suite *TestSuite) TestDatabaseConfig() {
+	config := config.NewDatabaseConfiguration()
+	config.Host = "localhost"
+	config.Port = "5243"
+	config.Name = "moonbase"
+	config.Username = "billgates"
+	config.Password = "god"
+
+	expected := "postgres://billgates:god@localhost:5243/moonbase"
+	assert.Equal(suite.T(), expected, config.URI())
+
+	config.Password = ""
+	expected = "postgres://billgates@localhost:5243/moonbase"
+	assert.Equal(suite.T(), expected, config.URI())
+
+	config.Username = ""
+	expected = "postgres://localhost:5243/moonbase"
+	assert.Equal(suite.T(), expected, config.URI())
+
+	config.Port = ""
+	expected = "postgres://localhost/moonbase"
+	assert.Equal(suite.T(), expected, config.URI())
+}
+
 
 func TestConfigurationTestSuite(t *testing.T) {
 	suite.Run(t, new(TestSuite))
