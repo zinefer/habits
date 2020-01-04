deea7017f1fb57a60f9944955654756536f6081d
zinefer <zinefer@gmail.com>
2020-01-04 11:11:03 -0700

    Make a nice range of colors in habit_calendar

--- web/src/components/habit_calendar.vue
+++ web/src/components/habit_calendar.vue
@@ -22,12 +22,6 @@
         />
       </g>
 
-      <!--i = 0
-      0, 6
-
-      i = 1
-      7, 13-->
-
       <!--<g class="calendar-week" v-for="(week, i) in values" :key="i">
         <rect
           class="calendar-day"
@@ -63,37 +57,6 @@ const DEFAULT_RANGE_COLOR = [
   "#196127" // 11+
 ];
 
-//const arrAvg = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
-
-/*function quickselect_median(arr) {
-  const L = arr.length,
-    halfL = L / 2;
-  if (L % 2 == 1) return quickselect(arr, halfL);
-  else return 0.5 * (quickselect(arr, halfL - 1) + quickselect(arr, halfL));
-}
-
-function quickselect(arr, k) {
-  // Select the kth element in arr
-  // arr: List of numerics
-  // k: Index
-  // return: The kth element (in numerical order) of arr
-  if (arr.length == 1) return arr[0];
-  else {
-    const pivot = arr[0];
-    const lows = arr.filter(e => e < pivot);
-    const highs = arr.filter(e => e > pivot);
-    const pivots = arr.filter(e => e == pivot);
-    if (k < lows.length)
-      // the pivot is too high
-      return quickselect(lows, k);
-    else if (k < lows.length + pivots.length)
-      // We got lucky and guessed the median
-      return pivot;
-    // the pivot is too low
-    else return quickselect(highs, k - lows.length - pivots.length);
-  }
-}*/
-
 export default {
   name: "HabitCalendar",
   props: {
@@ -109,6 +72,7 @@ export default {
   data() {
     return {
       max: 1,
+      groups: [],
       values: [],
       squareSize: 0
     };
@@ -117,30 +81,19 @@ export default {
     color(count) {
       if (count == 0) return this.rangeColors[0];
       //var index = Math.round((count + (this.max * 0.25) / this.max) * 4)
-      var index = Math.round((count / this.max) * 4 + 0.49);
+      var index = Math.round((count / this.max) * 4);
       if (index < 1) return this.rangeColors[1];
+      if (index > 4) return this.rangeColors[4];
       return this.rangeColors[index];
-    },
-    contribCount(count) {
-      return count >= this.rangeColors.length
-        ? this.rangeColors.length - 1
-        : count;
     }
   },
   mounted() {
     ActivitiesApi.get(this.habitID)
       .then(resp => {
         this.values = resp.data;
-        this.max =
-          Math.max.apply(
-            Math,
-            this.values.map(function(o) {
-              return o.Count;
-            })
-          ) || 1;
 
-        /*var filtered = this.values
-          .filter(value => value.Count > 0)
+        var filtered = this.values
+          .filter(value => value.Count > 1)
           .map(function(value) {
             return value.Count;
           });
@@ -148,27 +101,14 @@ export default {
         var sorted = filtered.sort(function(a, b) {
           return a - b;
         });
-        var pieces = 4;
-        var size = Math.floor(sorted.length / pieces);
-        for (let index = 0; index < pieces; index++) {
-          let start = index * size;
-          let end = index * size + size;
-          if (index == pieces - 1) {
-            end = sorted.length;
-          }
 
-          //let v = sorted.slice(start, end);
-          //let median = v[Math.round((end - start) / 2)];
-          //let avg = arrAvg(v);
-        }*/
+        var len = sorted.length;
+        this.max = sorted[Math.round(len * 0.95)];
       })
       .finally(() => {
         this.loading = false;
       });
 
-    /*this.values = data.data.user.contributionsCollection.contributionCalendar.weeks.slice(
-      53 - this.months * 4
-    );*/
     this.squareSize = this.$refs.calendar.offsetWidth / 55;
   }
 };
