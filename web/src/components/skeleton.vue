ebe8144285d804d407c389e4f6b1897b4f57eb08
zinefer <zinefer@gmail.com>
2020-01-05 23:03:04 -0700

    Create skeleton loader wrapper

---
+++ web/src/components/skeleton.vue
@@ -0,0 +1,35 @@
+<script>
+import { VSkeletonLoader } from "vuetify/lib";
+
+export default {
+  functional: true,
+  render(h, { data, props, children }) {
+    return h(
+      VSkeletonLoader,
+      {
+        ...data,
+        props: {
+          types: {
+            "habit-calendar-days": "avatar@132"
+          },
+          ...props
+        }
+      },
+      children
+    );
+  }
+};
+</script>
+
+<style lang="scss">
+.v-skeleton-loader__habit-calendar-days {
+  .v-skeleton-loader__avatar {
+    display: inline-block;
+    margin-right: 10px;
+    margin-bottom: 4px;
+    border-radius: 4px;
+    height: 42px;
+    width: 42px;
+  }
+}
+</style>
